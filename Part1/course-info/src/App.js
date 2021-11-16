bb654a4c0c69baff8e23946a5172720638f11fe8
princewhyte dabotubo <princewhyte02@gmail.com>
2021-11-16 21:49:27 +0100

    course-info

---
+++ Part1/course-info/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Header = ({ course }) => <h1>{ course }</h1>
+
+
+const Part = ({ part }) => (<p>{ part.name } { part.exercises }</p>)
+
+const Content = ({ parts }) =>
+(
+  <div>
+    { parts.map((part, index) => <Part key={ index } part={ part } />) }
+  </div>
+)
+
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
+  return <p>Number of exercises { total }</p>
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={ course.name } />
+      <Content parts={ course.parts } />
+      <Total parts={ course.parts } />
+    </div>
+  )
+}
+
+export default App
